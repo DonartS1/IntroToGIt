f6fcebf6062c9c5db2aabea70a2c25e1e3425c97
Donart Sylejmani <sylejmani.donart@gmail.com>
2024-12-13 10:53:41 +0100

    First Commit me Grupin e pare

---
+++ untitled/src/Gt1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Gt1 {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
